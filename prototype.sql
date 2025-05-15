c13918e545404dc359af765461572211fc012ec0
persianass.fla <110363190+thatpersianass@users.noreply.github.com>
2025-05-15 15:59:35 +0100

    i forgor the database again fukkk

--- prototype.sql
+++ prototype.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2025 a las 14:24:34
+-- Tiempo de generación: 15-05-2025 a las 16:59:02
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -43,9 +43,11 @@ CREATE TABLE `files` (
 --
 
 INSERT INTO `files` (`id`, `real_name`, `name`, `type`, `user_id`, `size`, `date`, `folder_id`) VALUES
-(1, '6825c5ce4c8618.63552584.pdf', 'FacturaMembrete.pdf', 'pdf', 5796, 129150, '2025-05-15 10:45:34', 1),
-(3, '6825ce6e8f6ad5.78335893.pdf', 'Resumen.pdf', 'pdf', 5796, 40671, '2025-05-15 11:22:22', 1),
-(4, '6825cf11594066.28563294.pdf', 'FacturaSencilla.pdf', 'pdf', 5796, 100717, '2025-05-15 11:25:05', 2);
+(1, '6825e415c6a543.72671885.pdf', 'Factura 1.pdf', 'pdf', 95666, 3943, '2025-05-15 12:54:45', 1),
+(2, '6825fa467a5475.03473876.pdf', 'Factura 2.pdf', 'pdf', 95666, 30134, '2025-05-15 14:29:26', 1),
+(3, '6825fdc77731b2.10982496.pdf', 'Funciona??.pdf', 'pdf', 95666, 3398721, '2025-05-15 14:44:23', 1),
+(4, '6825fe1cebe0e9.35371948.pdf', 'Nomina-Marzo.pdf', 'pdf', 7813, 69652, '2025-05-15 14:45:48', 2),
+(5, '6825ff67887b37.89967321.jpg', 'Cliente.jpg', 'jpg', 95666, 25676, '2025-05-15 14:51:19', 1);
 
 -- --------------------------------------------------------
 
@@ -64,11 +66,9 @@ CREATE TABLE `folders` (
 --
 
 INSERT INTO `folders` (`id`, `user_id`, `name`) VALUES
-(1, 5796, 'Documentos'),
-(2, 5796, 'Facturas'),
-(4, 5796, 'Contratos'),
-(5, 5796, 'Nominas'),
-(6, 5796, 'Horarios');
+(1, 95666, 'Facturas'),
+(2, 7813, 'Documentos'),
+(3, 95666, 'Documentos');
 
 -- --------------------------------------------------------
 
@@ -93,8 +93,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_id`, `username`, `passwd`, `name`, `1surname`, `2surname`, `dni`, `admin`) VALUES
-(1, 1, 'admin', 'admin', NULL, NULL, NULL, NULL, 1),
-(3, 5796, 'marcosJ', '3003', 'Marcos Javier', 'Pérez', 'Gómez', '43857678J', 0);
+(1, 1, 'admin', '$2y$10$ZF6AYQrpL6X.MKgcyolXme2Ipk3B5gh5XXu31ipLBkmP8CMRkmmba', '', '', '', '', 1),
+(12, 95666, 'marcosj', '$2y$10$AZeGB5ojE7PsLMS0rPlZTe7P1WWQKpcVIZ6Wn5TQekhyhs8cOdi3C', 'Marcos Javier', 'Pérez', 'Gómez', '43857678J', 0),
+(13, 7813, 'alexpd', '$2y$10$Fw7PaWPx0X8Vfzz3PDazHOlypXc73dB9zOzrzw9mxBU31306P3qva', 'Alexander', 'Pérez', 'Dominguez', '12345678A', 0);
 
 --
 -- Índices para tablas volcadas
@@ -104,9 +105,7 @@ INSERT INTO `users` (`id`, `user_id`, `username`, `passwd`, `name`, `1surname`, 
 -- Indices de la tabla `files`
 --
 ALTER TABLE `files`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `folder_id` (`folder_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `folders`
@@ -131,30 +130,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `files`
 --
 ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `folders`
 --
 ALTER TABLE `folders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `files`
---
-ALTER TABLE `files`
-  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `files_ibfk_2` FOREIGN KEY (`folder_id`) REFERENCES `folders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `folders`
