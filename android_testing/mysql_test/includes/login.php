95813dd76ccb264e60ccbe70664b120e2d3d501e
persianass.fla <110363190+thatpersianass@users.noreply.github.com>
2025-06-02 12:54:04 +0100

    type shii

--- android_testing/mysql_test/includes/login.php
+++ android_testing/mysql_test/includes/login.php
@@ -1,12 +1,12 @@
 <?php
-if(isset($_POST['email']) && isset($_POST['password'])){
+if(isset($_POST['username']) && isset($_POST['password'])){
     file_put_contents("login_debug.log", print_r($_POST, true), FILE_APPEND);
     require_once "connection.php";
     require_once "validate.php";
 
-    $email = validate($_POST['email']);
+    $email = validate($_POST['username']);
     $password = validate($_POST['password']);
-    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '".md5($password)."'";
+    $sql = "SELECT * FROM users WHERE username = '$email' AND password = '".md5($password)."'";
     file_put_contents("login_debug.log", $sql . "\n", FILE_APPEND);
     $result = $con->query($sql);
     if($result->num_rows > 0){
---
+++ catWeb/android/login.php
@@ -0,0 +1,35 @@
+<?php
+if(isset($_POST['username']) && isset($_POST['password'])){
+    file_put_contents("login_debug.log", print_r($_POST, true), FILE_APPEND);
+    require_once "connection.php";
+
+    $username = $_POST['username'];
+    $passwd = $_POST['password'];
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    file_put_contents("login_debug.log", $sql . "\n", FILE_APPEND);
+    $result = $con->query($sql);
+
+    if($result->num_rows > 0){
+        // echo "success";
+        $user = $result->fetch_assoc();
+            if (password_verify($passwd, $user['passwd'])) {
+                // $_SESSION['username'] = $user['username'];
+                // $_SESSION['color'] = $user['color'];
+                // $_SESSION['is_admin'] = $user['admin'];
+                
+                // if ($user['admin']) {
+                //     echo "admin";
+                // } else {
+                //     echo "user";
+                // }
+                // exit();
+                echo "success";
+            } else {
+                echo "failure";
+            }
+    } else {
+        echo "failure";
+    }
+}
+
+?>
